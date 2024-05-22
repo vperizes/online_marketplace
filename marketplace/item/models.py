d9f010553285b7e898c3df74f5799fb18f2ec8cb
vperizes <vperizes@gmail.com>
2024-05-22 00:24:52 -0500

    add category table

--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ("name",)
+
+    # overrides default str rep -> returns the value of name
+    def __str__(self):
+        return self.name
