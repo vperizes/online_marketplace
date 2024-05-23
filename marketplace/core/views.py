7d7364a5b5f5574babe779ea6ca86606750cf2b3
vperizes <vperizes@gmail.com>
2024-05-23 15:08:15 -0500

    add html to front page to display items and categories. pass data to request

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 
 def index(request):
-    return render(request, "core/index.html")
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)[0:6]
+    return render(
+        request, "core/index.html", {"categories": categories, "items": items}
+    )
 
 
 def contact(request):
