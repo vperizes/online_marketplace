8cb3e56173ff5f7f5d726e673947fcfcf05a289a
vperizes <vperizes@gmail.com>
2024-05-24 16:18:21 -0500

    clean up url patterns

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -19,12 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),
-    path("contact/", views.contact, name="contact"),
+    path("", include("core.urls")),
     path("items/", include("item.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
