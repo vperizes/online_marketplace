dfb2962e4c4dc0deb327d8892f2673facc86e86a
vperizes <vperizes@gmail.com>
2024-05-24 23:54:44 -0500

    add login page

--- marketplace/core/forms.py
+++ marketplace/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
@@ -40,6 +40,26 @@ class SignupForm(UserCreationForm):
         widget=forms.PasswordInput(
             attrs={
                 "placeholder": "Repeat Password",
+                "class": "w-full py-2 px-6 rounded-xl",
+            }
+        )
+    )
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Your username",
+                "class": "w-full py-2 px-6 rounded-xl",
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
                 "class": "w-full py-2 px-6 rounded-xl",
             }
         )
