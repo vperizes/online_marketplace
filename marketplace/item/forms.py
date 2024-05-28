f5dd94c50efd8bd017c667314b816d8f2159afff
vperizes <vperizes@gmail.com>
2024-05-28 14:26:47 -0500

    add widgets/atrributes styling to new item form

--- marketplace/item/forms.py
+++ marketplace/item/forms.py
@@ -1,8 +1,18 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Item
 
+input_classes = "w-full py-4 px-6 rounded-xl border"
 
-class NewItemForm(ModelForm):
+
+class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ["category", "name", "description", "price", "image"]
+
+        widgets = {
+            "category": forms.Select(attrs={"class": input_classes}),
+            "name": forms.TextInput(attrs={"class": input_classes}),
+            "description": forms.Textarea(attrs={"class": input_classes}),
+            "price": forms.TextInput(attrs={"class": input_classes}),
+            "image": forms.FileInput(attrs={"class": input_classes}),
+        }
